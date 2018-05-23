c5d71b6c25627287e4ed4036f9e79447757dd79a
Anny Patel <30823493+annypatel@users.noreply.github.com>
2018-05-23 11:28:39 +0530

    Adds html body reader

--- crawler-reader/src/test/java/com/vertaperic/crawler/reader/HtmlTestData.java
+++ crawler-reader/src/test/java/com/vertaperic/crawler/reader/HtmlTestData.java
@@ -16,7 +16,7 @@ class HtmlTestData {
                 "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
                 "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
                 "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
-                "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n" +
+                "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
                 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium " +
                 "doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore " +
                 "veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim " +
